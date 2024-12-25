42c03b1943c226b3ea3516b03d94eefbc98f259f
dornech <dornech@users.noreply.github.com>
2024-12-25 23:48:51 +0100

    fix: clean __init__.py to avoid Hatch dependency

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.pkg_name}}/__init__.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.pkg_name}}/__init__.py
@@ -1,10 +1,8 @@
-"""Ultimate Notion provides a pythonic, high-level API for Notion
+# version determination
 
-Notion-API: https://developers.notion.com/reference/intro
-"""
 
-from importlib.metadata import PackageNotFoundError, version
-
+# original Hatchlor version
+# from importlib.metadata import PackageNotFoundError, version
 # try:
 #     __version__ = version('{{ cookiecutter.project_slug }}')
 # except PackageNotFoundError:  # pragma: no cover
@@ -12,25 +10,37 @@ from importlib.metadata import PackageNotFoundError, version
 # finally:
 #     del version, PackageNotFoundError
 
+# modified version for editable mode installs (if desired
 # up-to-date version tag for modules installed in editable mode inspired by
 # https://github.com/maresb/hatch-vcs-footgun-example/blob/main/hatch_vcs_footgun_example/__init__.py
 # Define the variable '__version__':
+# try:
+#
+#     # own developed alternative variant to hatch-vcs-footgun overcoming problem of ignored setuptools_scm settings
+#     # from hatch-based pyproject.toml libraries
+#     from hatch.cli import hatch
+#     from click.testing import CliRunner
+#     # determine version via hatch
+#     __version__ = CliRunner().invoke(hatch, ["version"]).output.strip()
+#
+# except (ImportError, LookupError):
+#     # As a fallback, use the version that is hard-coded in the file.
+#     try:
+#         from ._version import __version__  # noqa: F401
+#     except ModuleNotFoundError:
+#         # The user is probably trying to run this without having installed the
+#         # package, so complain.
+#         raise RuntimeError(
+#             f"Package {__package__} is not correctly installed. Please install it with pip."
+#         )
+
+# simplified version to avoid evaluation of metadata and additional dependencies
+# version file is generated during Hatch build process
 try:
-
-    # own developed alternative variant to hatch-vcs-footgun overcoming problem of ignored setuptools_scm settings
-    # from hatch-based pyproject.toml libraries
-    from hatch.cli import hatch
-    from click.testing import CliRunner
-    # determine version via hatch
-    __version__ = CliRunner().invoke(hatch, ["version"]).output.strip()
-
-except (ImportError, LookupError):
-    # As a fallback, use the version that is hard-coded in the file.
-    try:
-        from ._version import __version__  # noqa: F401
-    except ModuleNotFoundError:
-        # The user is probably trying to run this without having installed the
-        # package, so complain.
-        raise RuntimeError(
-            f"Package {__package__} is not correctly installed. Please install it with pip."
-        )
+    from ._version import __version__  # noqa: F401
+except ModuleNotFoundError:
+    # The user is probably trying to run this without having installed the
+    # package, so complain.
+    raise RuntimeError(
+        f"Package {__package__} is not correctly installed. Please install it with pip."
+    )
